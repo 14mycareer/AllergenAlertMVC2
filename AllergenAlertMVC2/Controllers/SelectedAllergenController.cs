f4154bd4f8444a6d5ffe0a3bd9a5b67681de7f78
Rakiyah Faatuono <rakiyahfaatuono@gmail.com>
2019-12-10 19:20:41 -0600

    modify selectedallergenviewmodel to get display of allergens

--- AllergenAlertMVC2/Controllers/SelectedAllergenController.cs
+++ AllergenAlertMVC2/Controllers/SelectedAllergenController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,8 +21,8 @@ namespace AllergenAlertMVC2.Controllers
 
         //method to retrieve selected allergen from dropdownlist
 
-        [HttpPost]
-        public IActionResult (allergenList)
+       // [HttpPost]
+        //public IActionResult (allergenList)
     
 
     }
--- AllergenAlertMVC2/ViewModels/SelectedAllergenViewModel.cs
+++ AllergenAlertMVC2/ViewModels/SelectedAllergenViewModel.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
+﻿using AllergenAlertMVC2.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AllergenAlertMVC2.ViewModels
 {
@@ -17,10 +15,10 @@ namespace AllergenAlertMVC2.ViewModels
         public SelectedAllergenViewModel()
         {
             AllergenTypes = new List<SelectListItem>();
-            AllergenTypes.Add(new SelectListItem
+            AllergenTypes.Add(new SelectListItem()
             {
-                Value = ((int)AllergenType.Nuts).ToString(),
-                Text = AllergenType.Nuts.ToString()
+                Value ="0",// ((int)AllergenType.Nuts).ToString(),
+                Text ="nuts" //AllergenType.Nuts.ToString()
             });
         }
     }
